52ad4274f1c5f800bdf0a54ec5c7ddb45b798dd5
Kelly <kellywx12@gmail.com>
2024-10-04 16:30:57 -0700

    Added implementation for WishList class

--- src/main/model/WishList.java
+++ src/main/model/WishList.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 // Represents a wishlist which contains ArrayList of Wish objects
 public class WishList {
@@ -12,32 +13,21 @@ public class WishList {
     }
 
     public void addWish(String name, String brand, int price) {
-        // stub
+        wishList.add(new Wish(name, brand, price));
     }
 
-    public void deleteWish(String name, String brand, int price) {
-        // stub
+    public void deleteWish(String name, String brand) {
+        Iterator<Wish> iterator = wishList.iterator();
+        // Iterate through the wishlist
+        while (iterator.hasNext()) {
+            Wish wish = iterator.next();
+            if (wish.getName().contains(name) && wish.getBrand().contains(brand)) {
+                iterator.remove();
+            }
+        };
     }
 
     public ArrayList<Wish> getWishList() {
         return wishList;
     }
-
-    // student completes task example in lecture
-    // got back list, verify tasks in list
-
-    // user class that constructs wishlist and to buy list, list to keep track of friends
-    // we can use java
-    // make sure to have loops and branch, if, switch loop th
-
-    // field with type WishList (clear WishList, get WishList) (test all methods in user class)
-
-    // another class to keep track of users
-
-    // user ID and password (two properties user would need to have), name,
-
-    // another class to just store the user, this class has method called authenticate where we pass in user ID and password that was typed in the keyboard, it returns user 
-    // create some fake users (populate the list)
-
-    // add item from friend's list to our own list
 }
