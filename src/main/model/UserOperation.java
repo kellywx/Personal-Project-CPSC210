bab7960f2f7d40384d4c5585413d9dbc479f2c54
Kelly <kellywx12@gmail.com>
2024-10-04 16:31:37 -0700

    Added implementation for UserOperation class

--- src/main/model/UserOperation.java
+++ src/main/model/UserOperation.java
@@ -11,19 +11,30 @@ public class UserOperation {
     // MODIFIES: this
     // EFFECTS: Registers a new user 
     public User registerUser(String username, String email, String password) {
-        // stub
+        if (users.containsKey(username)) {
+            return null;
+        }
+
+        User user = new User(users.size()+1, username, email, password);
+        users.put(username, user);
+        return user;
     }
 
     // MODIFIES: this
     // EFFECTS: Checks user's username and password, then authenticate
     public User loginUser(String username, String password) {
-        // stub
+        User user = users.get(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        // Failed to login
+        return null;
     }
 
     // MODIFIES: this
     // EFFECTS: Searches for a user using username
     public User findUserByUsername(String username) {
-        // stub
+        return users.get(username);
     }
 }
 
