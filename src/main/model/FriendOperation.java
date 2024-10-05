314afd154e8c2c342f2e3507838849625a46c871
Kelly <kellywx12@gmail.com>
2024-10-04 20:37:22 -0700

    Added implementation for FriendOperation class

--- src/main/model/FriendOperation.java
+++ src/main/model/FriendOperation.java
@@ -1,43 +1,50 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class FriendOperation {
     private List<FriendRequest> friendRequests = new ArrayList<>();
 
     // Store each user's list of friends
-    private Map<User, List<User>> friends = new HashMap<>();
+    private List<User> friends = new ArrayList<>();
 
     // MODIFIES: this
     // EFFECTS: constructs a new friend request from sender to receiver
     public void sendFriendRequest(User sender, User receiver) {
-        // stub
+        FriendRequest request = new FriendRequest(sender, receiver);
+        friendRequests.add(request);
     }
+
 
     // MODIFIES: this
     // EFFECTS: accepts friend request by setting status to "ACCEPTED"
     public void acceptFriendRequest(FriendRequest request) {
-        // stub
+        request.setStatus("ACCEPTED");
+        friends.add(request.getSender());
     }
 
     // MODIFIES: this
     // EFFECTS: denys friend request by setting status to "DENIED"
     public void denyFriendRequest(FriendRequest request) {
-        // stub
+        request.setStatus("DENIED");
     }
 
     // MODIFIES: this
     // EFFECTS: returns a list of user's friend requests with status "PENDING"
     public List<FriendRequest> getPending(User user) {
-        // stub
+        List<FriendRequest> pendingRequests = new ArrayList<>();
+        for (FriendRequest request : friendRequests) {
+            if (request.getReceiver().equals(user) && request.getStatus().equals("PENDING")) {
+                pendingRequests.add(request);
+            }
+        }
+        return pendingRequests;
     }
 
     // MODIFIES: this
     // EFFECTS: returns user's list of friends
     public List<User> getFriends(User user) {
-        // stub
+        return friends;
     }
 }
