5b9e088a9cc4428021e3f5152a861a0308f9dbfe
Kelly <kellywx12@gmail.com>
2024-10-09 14:40:56 -0700

    Edited tests for Wish and WishList classes

--- src/test/model/TestWish.java
+++ src/test/model/TestWish.java
@@ -2,7 +2,7 @@ package model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
--- src/test/model/TestWishList.java
+++ src/test/model/TestWishList.java
@@ -1,6 +1,8 @@
 package model;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,22 +17,24 @@ public class TestWishList {
 
     @Test
     void testConstructor() {
-        assertEquals(0, testWishList.getWishList().size());
+        assertTrue(testWishList.getWishList().isEmpty());
     }
 
     @Test
     void testAddWish() {
         testWishList.addWish("Lipstick", "Rare", 30);
         testWishList.addWish("T-shirt", "Lululemon", 20);
+        assertFalse(testWishList.getWishList().isEmpty());
         assertEquals(2, testWishList.getWishList().size());
+        assertEquals("Lipstick", testWishList.getWishList().get(0).getName());
+        assertEquals(20, testWishList.getWishList().get(1).getPrice());
     }
 
     @Test
     void testDeleteWish() {
         testWishList.addWish("Lipstick", "Rare", 30);
-        testWishList.addWish("T-shirt", "Lululemon", 20);
-        testWishList.deleteWish("Lipstick", "Rare", 20);
-        assertEquals(1, testWishList.getWishList().size());
+        testWishList.deleteWish("Lipstick", "Rare");
+        assertTrue(testWishList.getWishList().isEmpty());
     }
 
 }
