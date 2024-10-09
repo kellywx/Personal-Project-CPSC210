77e3665c9a118f679d32872b1343ab750d2cc092
Kelly <kellywx12@gmail.com>
2024-10-09 13:52:06 -0700

    Added specification for Friend class

---
+++ src/main/model/Friend.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Represents a friend profile
+public class Friend {
+    private String name;
+    private WishList toBuyList;
+
+    // EFFECTS: constructs a friend with given name
+    //          and empty to-buy list
+    public Friend(String name) {
+        
+    }
+}
