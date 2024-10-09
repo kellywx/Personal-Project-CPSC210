c4c7375ff876255dd92caf13de184aa3f407685b
Kelly <kellywx12@gmail.com>
2024-10-09 14:59:43 -0700

    Added more tests for WishList class

--- src/test/model/TestWishList.java
+++ src/test/model/TestWishList.java
@@ -24,17 +24,36 @@ public class TestWishList {
     void testAddWish() {
         testWishList.addWish("Lipstick", "Rare", 30);
         testWishList.addWish("T-shirt", "Lululemon", 20);
+        
+        // Testing if wishlist has 2 items, not empty
         assertFalse(testWishList.getWishList().isEmpty());
         assertEquals(2, testWishList.getWishList().size());
+
+        // Testing if the first item and second item have been added
         assertEquals("Lipstick", testWishList.getWishList().get(0).getName());
         assertEquals(20, testWishList.getWishList().get(1).getPrice());
     }
 
     @Test
     void testDeleteWish() {
+        testWishList.addWish("T-shirt", "Lululemon", 20);
         testWishList.addWish("Lipstick", "Rare", 30);
+
+        // Testing if both name and brand match the item
         testWishList.deleteWish("Lipstick", "Rare");
-        assertTrue(testWishList.getWishList().isEmpty());
+        assertEquals(1, testWishList.getWishList().size());
+
+        // Testing if name matches, but brand does not match
+        testWishList.deleteWish("T-shirt", "Nike");
+        assertEquals(1, testWishList.getWishList().size());
+
+        // Testing if brand matches, but name does not match
+        testWishList.deleteWish("Shoes", "Nike");
+        assertEquals(1, testWishList.getWishList().size());
+
+        // Testing if both name and brand do not match
+        testWishList.deleteWish("Laptop", "Apple");
+        assertEquals(1, testWishList.getWishList().size());
     }
 
 }
