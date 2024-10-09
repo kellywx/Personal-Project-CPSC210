01bb194cfb23c374cb90db18308b5b376a9e57e3
Kelly <kellywx12@gmail.com>
2024-10-09 14:58:39 -0700

    Added tests for Friend class

---
+++ src/test/model/TestFriend.java
@@ -0,0 +1,22 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestFriend {
+    private Friend testFriend;
+
+    @BeforeEach
+    void runBefore() {
+        testFriend = new Friend("Amy");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Amy", testFriend.getName());
+        assertTrue(testFriend.getToBuyList().getWishList().isEmpty());
+    }
+}
