615d63f9c4fdefd57ad61dc5ae7ba1b294329810
Kelly <kellywx12@gmail.com>
2024-10-20 15:21:22 -0700

    Added specification for JsonWriter class

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,41 @@
+package persistence;
+import org.json.JSONObject;
+
+import model.WishList;
+
+import java.io.*;
+
+// Represents a writer that writes JSON representation of wishlist to file
+public class JsonWriter {
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of wishlist to file
+    public void write(WishList wishList) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        // stub
+    }
+}
+
