e22ca909a5b456ef27a6ab1a9bdfa2154258dd67
Kelly <kellywx12@gmail.com>
2024-10-23 10:46:28 -0700

    Checks checkedOff in JsonTest

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import model.Wish;
 
 public class JsonTest {
-    protected void checkWish(String name, String brand, int price, Wish wish) {
+    protected void checkWish(String name, String brand, int price, String checkedOff, Wish wish) {
         assertEquals(name, wish.getName());
         assertEquals(brand, wish.getBrand());
         assertEquals(price, wish.getPrice());
+        assertEquals(checkedOff, wish.isCheckedOff());
     }
 }
 
