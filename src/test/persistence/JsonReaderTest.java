61416c82e98c0696ec9bf32bb936499efed55210
Kelly <kellywx12@gmail.com>
2024-10-23 10:44:58 -0700

    Added more tests for JsonReader

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -2,8 +2,11 @@ package persistence;
 
 import org.junit.jupiter.api.Test;
 
+import model.FriendList;
+import model.Friend;
 import model.Wish;
 import model.WishList;
+import model.Wallet;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,7 +19,9 @@ class JsonReaderTest extends JsonTest {
     void testReaderNonExistentFile() {
         JsonReader reader = new JsonReader("./data/noSuchFile.json");
         try {
-            WishList wishList = reader.read();
+            WishList wishList = reader.readWishList();
+            FriendList friendList = reader.readFriendList();
+            Wallet wallet = reader.readWallet();
             fail("IOException expected");
         } catch (IOException e) {
             // pass
@@ -27,9 +32,13 @@ class JsonReaderTest extends JsonTest {
     void testReaderEmptyWishList() {
         JsonReader reader = new JsonReader("./data/testReaderEmptyWishList.json");
         try {
-            WishList wishList = reader.read();
-            assertEquals("My Wishlist", wishList.getName());
+            WishList wishList = reader.readWishList();
+            FriendList friendList = reader.readFriendList();
+            Wallet wallet = reader.readWallet();
             assertEquals(0, wishList.getWishList().size());
+            assertEquals(0, friendList.getFriendList().size());
+            assertEquals(0, wallet.getMoney());
+
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
@@ -39,12 +48,18 @@ class JsonReaderTest extends JsonTest {
     void testReaderGeneralWishList() {
         JsonReader reader = new JsonReader("./data/testReaderGeneralWishList.json");
         try {
-            WishList wishList = reader.read();
-            assertEquals("My Wishlist", wishList.getName());
+            WishList wishList = reader.readWishList();
             ArrayList<Wish> wishes = wishList.getWishList();
             assertEquals(2, wishes.size());
-            checkWish("Shoes", "Nike", 90, wishes.get(0));
-            checkWish("Laptop", "Apple", 2500, wishes.get(1));
+            checkWish("Shoes", "Nike", 90, "No", wishes.get(0));
+            checkWish("Laptop", "Apple", 2500, "Yes", wishes.get(1));
+            FriendList friendList = reader.readFriendList();
+            ArrayList<Friend> friends = friendList.getFriendList();
+            assertEquals(1, friends.size());
+            ArrayList<Wish> monicaWishes = friends.get(0).getToBuyList().getWishList();
+            assertEquals(1, monicaWishes.size());
+            checkWish("Shirt", "Ralph Lauren", 30, "No",monicaWishes.get(0));
+
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
